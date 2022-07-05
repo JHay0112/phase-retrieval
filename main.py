e7ba0240ada388bdd680ea58c735fbd922335485
JHay0112 <jordanhay0112@gmail.com>
2022-07-05 22:34:22 +1200

    Setup for loading in ground truth

--- main.py
+++ main.py
@@ -20,7 +20,39 @@
 import numpy as np
 from numpy.fft import fft2
 
-'''
+from PIL import Image, ImageOps
 
-'''
-BETA = 0.5
+
+
+BETA = 0.5 # Difference Map Parameter, interpolates between constraints
+PADDING = 2 # Zero padding on guess, ensures that solution converges properly
+
+IMG_PATH = "img/"
+
+
+
+def fourier_modulus(image: np.array) -> np.array:
+    '''
+        Computes the Fourier Modulus of a given image (2D array).
+
+        Parameters
+        ----------
+
+        image: np.array
+            The image to compute the fourier modulus of.
+
+        Returns
+        -------
+
+        np.array
+            Fourier Modulus of the passed image.
+    '''
+    return np.abs(fft2(image))
+
+
+
+if __name__ == "__main__":
+
+    true_img = Image.open(IMG_PATH)
+    true_img = ImageOps.grayscale(true_img)
+
